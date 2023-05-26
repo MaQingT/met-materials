1f9c48967a916e56c564be5f3be75eaadfa50674
qingtao.ma <qingtao.ma@dena.com>
2023-05-26 10:38:28 +0800

    Update chapter 11

--- 11-maps-and-materials/projects/starter/Materials/Shared/Shaders/Common.h
+++ 11-maps-and-materials/projects/starter/Materials/Shared/Shaders/Common.h
@@ -36,57 +36,72 @@
 #import <simd/simd.h>
 
 typedef struct {
-  matrix_float4x4 modelMatrix;
-  matrix_float4x4 viewMatrix;
-  matrix_float4x4 projectionMatrix;
-  matrix_float3x3 normalMatrix;
+    matrix_float4x4 modelMatrix;
+    matrix_float4x4 viewMatrix;
+    matrix_float4x4 projectionMatrix;
+    matrix_float3x3 normalMatrix;
 } Uniforms;
 
 typedef struct {
-  uint width;
-  uint height;
-  uint tiling;
-  uint lightCount;
-  vector_float3 cameraPosition;
+    uint width;
+    uint height;
+    uint tiling;
+    uint lightCount;
+    vector_float3 cameraPosition;
 } Params;
 
 typedef enum {
-  Position = 0,
-  Normal = 1,
-  UV = 2,
-  Color = 3
+    Position = 0,
+    Normal = 1,
+    UV = 2,
+    Color = 3,
+    Tangent = 4,
+    Bitangent = 5
 } Attributes;
 
 typedef enum {
-  VertexBuffer = 0,
-  UVBuffer = 1,
-  ColorBuffer = 2,
-  UniformsBuffer = 11,
-  ParamsBuffer = 12,
-  LightBuffer = 13
+    VertexBuffer = 0,
+    UVBuffer = 1,
+    ColorBuffer = 2,
+    UniformsBuffer = 11,
+    ParamsBuffer = 12,
+    LightBuffer = 13,
+    TangentBuffer = 3,
+    BitangentBuffer = 4,
+    MaterialBuffer = 14
 } BufferIndices;
 
 typedef enum {
-  BaseColor = 0
+    BaseColor = 0,
+    NormalTexture = 1,
 } TextureIndices;
 
 typedef enum {
-  unused = 0,
-  Sun = 1,
-  Spot = 2,
-  Point = 3,
-  Ambient = 4
+    unused = 0,
+    Sun = 1,
+    Spot = 2,
+    Point = 3,
+    Ambient = 4
 } LightType;
 
 typedef struct {
-  vector_float3 position;
-  vector_float3 color;
-  vector_float3 specularColor;
-  vector_float3 attenuation;
-  LightType type;
-  float coneAngle;
-  vector_float3 coneDirection;
-  float coneAttenuation;
+    vector_float3 position;
+    vector_float3 color;
+    vector_float3 specularColor;
+    vector_float3 attenuation;
+    LightType type;
+    float coneAngle;
+    vector_float3 coneDirection;
+    float coneAttenuation;
 } Light;
+
+typedef struct{
+    vector_float3 baseColor;
+    vector_float3 specularColor;
+    float roughness;
+    float metallic;
+    float ambientOcclusion;
+    float shininess;
+} Material;
 
 #endif /* Common_h */
